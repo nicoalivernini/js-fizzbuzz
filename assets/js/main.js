35d13959eff83110e8e479755483c36c16861af0
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-05 16:40:57 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,18 @@
-// Come faccio a sapere se un numero è divisibile per?
-// Abbiamo visto qualcosa di particolare che possiamo usare?
-
-
-/*
-
-var numeri = [];
-
-// Programma che stampa numeri da 1 a 100
-for (var i = 1; i < 100 + 1; i++) {
-  numeri = [i];
-  document.getElementById('numeri').innerHTML += "<li>" + numeri + "</li>"
-
-  //Multipli di 3 stampiamo "Fizz" al posto del numero - possiamo utilizzare il %2?
-  if (numeri %3 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "Fizz" + "</li>"
-  } //Multipli di 5 stampiano "Buzz" al posto del numero
-    else if (numeri %5 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "Buzz" + "</li>"
-  }
-
-  // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-  if (numeri %3 == 0 && numeri %5 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>"
-  }
-}
-
-
-*/
-
-
-
-
 var i = 0;
 
 do {
-  document.getElementById('numeri').innerHTML += "<li>" + (i + 1)  + "</li>"
   i++;
-  //Multipli di 3 stampiamo "Fizz" al posto del numero - possiamo utilizzare il %2?
-  if (i %3 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "Fizz" + "</li>"
-  } //Multipli di 5 stampiano "Buzz" al posto del numero
+  // Se i numeri sono sia multipli di 3 che di 5 stampi FizzBuzz.
+  if (i %3 == 0 && i %5 == 0) {
+    document.getElementById('numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>";
+  } //Altrimenti se i numeri sono multipli di 3 stampiamo "Fizz" al posto del numero.
+    else if (i %3 == 0) {
+    document.getElementById('numeri').innerHTML += "<li>" + "Fizz" + "</li>";
+  } //Altrimenti se i numeri sono multipli di 5 stampiano "Buzz" al posto del numero
     else if (i %5 == 0) {
     document.getElementById('numeri').innerHTML += "<li>" + "Buzz" + "</li>"
+  } // Altrimenti se non sono multipli ne di 3 ne di 5 stampiamo il numero
+    else {
+    document.getElementById('numeri').innerHTML += "<li>" + i + "</li>"
   }
-
-  // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-  if (i %3 == 0 && i %5 == 0) {
-    document.getElementById('numeri').innerHTML += "<li>" + "FizzBuzz" + "</li>"
-  }
-
 } while (i < 100);
