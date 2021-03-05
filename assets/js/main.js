8482b479e4d65e1793632b3679029b45039940b4
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-05 15:17:13 +0100

    Creazione file di base

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+
+// Programma che stampa numeri da 1 a 100
+
+//Multipli di 3 stampiamo "Fizz" al posto del numero - possiamo utilizzare il %2?
+
+//Multipli di 5 stampiano "Buzz" al posto del numero
