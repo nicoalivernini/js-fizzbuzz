39aeb729aac83413500e2c6ca65005e526ae908b
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-05 17:51:13 +0100

    Esercizio completato + bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,30 @@ do {
     document.getElementById('numeri').innerHTML += "<li>" + i + "</li>"
   }
 } while (i < 100);
+
+
+// ESEGUO CON IL CIFLO FOR - BONUS
+
+var numeri = [];
+
+// Programma che stampa numeri da 1 a 100
+for (var i = 1; i < 100 + 1; i++) {
+  numeri = [i];
+    // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    if (numeri %3 == 0 && numeri %5 == 0) {
+      document.getElementById('for-bonus').innerHTML += "<li>" + "FizzBuzz" + "</li>"
+    }
+    //Multipli di 3 stampiamo "Fizz" al posto del numero - possiamo utilizzare il %2?
+    else if (numeri %3 == 0) {
+    document.getElementById('for-bonus').innerHTML += "<li>" + "Fizz" + "</li>"
+  } //Multipli di 5 stampiano "Buzz" al posto del numero
+    else if (numeri %5 == 0) {
+    document.getElementById('for-bonus').innerHTML += "<li>" + "Buzz" + "</li>"
+  } //Ai numeri non multipli di 3 / 5 stampiamo solo il nome
+    else {
+    document.getElementById('for-bonus').innerHTML += "<li>" + numeri + "</li>"
+
+  }
+
+
+}
